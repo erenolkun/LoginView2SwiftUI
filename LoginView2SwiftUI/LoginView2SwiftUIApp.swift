6ddbfde9c50c0af790fa30d51be871ab72124a92
erenolkun <erenolkun@gmail.com>
2023-09-14 10:46:05 +0300

    Initial Commit

---
+++ LoginView2SwiftUI/LoginView2SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginView2SwiftUIApp.swift
+//  LoginView2SwiftUI
+//
+//  Created by FERDA OLKUN on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginView2SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
